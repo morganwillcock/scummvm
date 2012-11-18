05cbec3534d78420048d264ca95f3fa75c7874c0
rofl0r <retnyg@gmx.net>
2012-11-18 06:32:55 +0100

    AGS: add score event
    with this change, duty and beyond is almost completely playable.
    only other thing missing so far is Object::get_View

--- engines/ags/ags.cpp
+++ engines/ags/ags.cpp
@@ -1786,6 +1786,7 @@ bool AGSEngine::runInteractionCommandList(NewInteractionEvent &event, NewInterac
 	for (uint i = 0; i < commands.size(); ++i) {
 		commandsRunCount++;
 		uint roomWas = _state->_roomChanges;
+		int32 temparg = 0;
 
 		const NewInteractionCommand &command = commands[i];
 		debug(6, "runInteractionCommandList: action %d", command._type);
@@ -1811,7 +1812,13 @@ bool AGSEngine::runInteractionCommandList(NewInteractionEvent &event, NewInterac
 			event._response->_timesRun++;
 			// fallthrough
 		case kActionAddScore:
-			// FIXME
+			temparg = (int32) commands[i]._args[0]._val;
+			this->_state->_score += temparg;
+			_guiNeedsUpdate = true;
+			if (temparg > 0 && (int)_state->_scoreSound >= 0) {
+				_audio->playAudioClipByIndex(_state->_scoreSound);
+			}
+			runOnEvent(GE_GOT_SCORE, temparg);
 			break;
 		case kActionDisplayMessage:
 			// FIXME
